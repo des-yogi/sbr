2b13557d46b0500fa9182c2d2a3f6ff9c0c41582
Igor <des.yogi@gmail.com>
2024-12-09 23:14:47 +0200

    part 1

--- src/blocks/review/review.js
+++ src/blocks/review/review.js
@@ -1,4 +1,15 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  const review = new Swiper('.review__slider', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 24,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  });
+}());
